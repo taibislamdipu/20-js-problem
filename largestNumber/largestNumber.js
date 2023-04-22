8af3cdc918a1dc45c745378e71859319bf820de4
taibislamdipu <mailtaibislam@gmail.com>
2023-04-22 18:54:54 +0600

    Find largest number in array

--- largestNumber/largestNumber.js
+++ largestNumber/largestNumber.js
@@ -1,2 +1,9 @@
 // Problem-2: Given an array of numbers, find the largest number.
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 10, 3, 4, 5];
+
+function largest(arr) {
+  const max = Math.max(...arr);
+  return max;
+}
+
+console.log(largest(numbers));
--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,3 +1,3 @@
 // Problem-2: Given an array of numbers, find the largest number.
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
